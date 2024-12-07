f474b894db5436d685ce0d44cb0dfe4dba07e3ae
yefeiyu <yefeiyu@gmail.com>
2024-12-07 19:11:07 +0800

    Initial Commit

---
+++ fortiger/fortigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fortigerApp.swift
+//  fortiger
+//
+//  Created by Charles Thomas on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct fortigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
